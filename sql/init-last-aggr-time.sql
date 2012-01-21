0416e60f97b8e54377661e0547fb4a59df017089
Tomasz Rozensztrauch <trozensztrauch@gmail.com>
2012-01-21 21:58:26 +0100

    init-last-aggr-time.sql: Fixed incorrect value
    Wrong value of last-aggr-time was being inserted intto current config

--- sql/init-last-aggr-time.sql
+++ sql/init-last-aggr-time.sql
@@ -1,5 +1,5 @@
 INSERT OR IGNORE INTO config (section_id, key, value) VALUES (
   (SELECT id FROM config_section WHERE name == 'current'),
-  'aggr-period-mins',
-  '15' 
+  'last-aggr-time',
+  SELECT CAST(strftime('%s', 'now') AS INTEGER) 
 );
