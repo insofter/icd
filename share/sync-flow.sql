542859eafa58085f94673426693603f61f5d5fce
Tomasz Rozensztrauch <trozensztrauch@gmail.com>
2012-09-12 11:13:52 +0200

    Added scripts for db syncing
    icd-sync-config - copies config into config.db
    icd-sync-flow - copies flow into flow.db
    icd-sync-db - executes both icd-sync-config and icd-sync-flow scripts

--- share/sync-flow.sql
+++ share/sync-flow.sql
@@ -17,18 +17,15 @@ UPDATE flow SET
   dark_time = (SELECT e.dark_time FROM externdb.flow e
     WHERE e.id == flow.id),
   work_time = (SELECT e.itd FROM externdb.flow e
-    WHERE e.id == flow.id),
-  test = (SELECT e.test FROM externdb.flow e
     WHERE e.id == flow.id)
   WHERE id IN (SELECT m.id FROM flow m JOIN externdb.flow e
     ON m.id == e.id
     WHERE m.itd IS NOT e.itd OR m.dtm IS NOT e.dtm OR m.cnt IS NOT e.cnt
-      OR m.dark_time IS NOT e.dark_time OR m.work_time IS NOT e.work_time
-      OR m.test IS NOT e.test);
+      OR m.dark_time IS NOT e.dark_time OR m.work_time IS NOT e.work_time);
 
 -- insert all records that are missing
-INSERT INTO flow (id, itd, dtm, cnt, dark_time, work_time, test)
-  SELECT e.id, e.itd, e.dtm, e.cnt, e.dark_time, e.work_time, e.test
+INSERT INTO flow (id, itd, dtm, cnt, dark_time, work_time)
+  SELECT e.id, e.itd, e.dtm, e.cnt, e.dark_time, e.work_time
     FROM externdb.flow e LEFT JOIN flow m ON m.id == e.id
     WHERE m.id IS NULL;
 
