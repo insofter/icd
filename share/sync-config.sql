b563ac4d261203a3533b26908ed78fbbab968225
Tomasz Rozensztrauch <trozensztrauch@gmail.com>
2012-09-19 20:01:09 +0200

    Fixed problems with db sync script.

--- share/sync-config.sql
+++ share/sync-config.sql
@@ -1,47 +1,14 @@
 -- main - the db that has outdated data
 -- extern -- the db that has up-to-date data
 
--- first step - config_section table
+DELETE FROM config;
+DELETE FROM config_section;
 
--- remove all records that exists in main db no longer exist in extern data;
--- this also removes all config records belonging to this section
-DELETE FROM config_section WHERE
- id IN (SELECT m.id FROM config_section m LEFT JOIN externdb.config_section e 
-   ON m.id == e.id WHERE e.id IS NULL);
+INSERT INTO config_section SELECT * FROM externdb.config_section e
+  WHERE e.name != "current";
 
--- update all records that are different; IS NOT is like '!=' but takes care of NULLs
-UPDATE config_section SET
-  name = (SELECT e.name FROM externdb.config_section e
-    WHERE e.id == config_section.id)
-  WHERE id IN (SELECT m.id FROM config_section m JOIN externdb.config_section e
-    ON m.id == e.id WHERE m.name IS NOT e.name);
-
--- insert all records that are missing
-INSERT INTO config_section (id, name)
-  SELECT e.id, e.name FROM externdb.config_section e LEFT JOIN config_section m
-    ON m.id == e.id WHERE m.id IS NULL;
-
--- second step - config table
-
--- remove all records that exists in main db no longer exist in extern data
-DELETE FROM config WHERE
- id IN (SELECT m.id FROM config m LEFT JOIN externdb.config e 
-   ON m.id == e.id WHERE e.id IS NULL);
-
--- update all records that are different; IS NOT is like '!=' but takes care of NULLs
-UPDATE config SET 
-  section_id = (SELECT e.section_id FROM externdb.config e
-    WHERE e.id == config.id),
-  key = (SELECT e.key FROM externdb.config e
-    WHERE e.id == config.id),
-  value = (SELECT e.value FROM externdb.config e
-    WHERE e.id == config.id)
-  WHERE id IN (SELECT m.id FROM config m JOIN externdb.config e
-    ON m.id == e.id
-    WHERE m.section_id IS NOT e.section_id OR m.key IS NOT e.key OR m.value IS NOT  e.value);
-
--- insert all records that are missing
-INSERT INTO config (id, section_id, key, value)
-  SELECT e.id, e.section_id, e.key, e.value FROM externdb.config e LEFT JOIN config m
-    ON m.id == e.id WHERE m.id IS NULL;
-
+INSERT INTO config SELECT * FROM externdb.config e
+  WHERE e.section_id != (SELECT x.id FROM externdb.config_section x
+    WHERE x.name == 'current');
+ 
+INSERT INTO config_section (name) VALUES ('current');
--- share/sync-flow.sql
+++ share/sync-flow.sql
@@ -8,25 +8,18 @@ DELETE FROM flow WHERE
 
 -- update all records that are different; IS NOT is like '!=' but takes care of NULLs
 UPDATE flow SET 
-  itd = (SELECT e.itd FROM externdb.flow e
-    WHERE e.id == flow.id),
-  dtm = (SELECT e.dtm FROM externdb.flow e
-    WHERE e.id == flow.id),
-  cnt = (SELECT e.cnt FROM externdb.flow e
-    WHERE e.id == flow.id),
-  dark_time = (SELECT e.dark_time FROM externdb.flow e
-    WHERE e.id == flow.id),
-  work_time = (SELECT e.itd FROM externdb.flow e
-    WHERE e.id == flow.id)
-  WHERE id IN (SELECT m.id FROM flow m JOIN externdb.flow e
-    ON m.id == e.id
+  itd = (SELECT e.itd FROM externdb.flow e WHERE e.id == flow.id),
+  dtm = (SELECT e.dtm FROM externdb.flow e WHERE e.id == flow.id),
+  cnt = (SELECT e.cnt FROM externdb.flow e WHERE e.id == flow.id),
+  dark_time = (SELECT e.dark_time FROM externdb.flow e WHERE e.id == flow.id),
+  work_time = (SELECT e.work_time FROM externdb.flow e WHERE e.id == flow.id),
+  flags = (SELECT e.flags FROM externdb.flow e WHERE e.id == flow.id)
+  WHERE id IN (SELECT m.id FROM flow m JOIN externdb.flow e ON m.id == e.id
     WHERE m.itd IS NOT e.itd OR m.dtm IS NOT e.dtm OR m.cnt IS NOT e.cnt
-      OR m.dark_time IS NOT e.dark_time OR m.work_time IS NOT e.work_time);
+      OR m.dark_time IS NOT e.dark_time OR m.work_time IS NOT e.work_time
+      OR m.flags IS NOT e.flags);
 
 -- insert all records that are missing
-INSERT INTO flow (id, itd, dtm, cnt, dark_time, work_time)
-  SELECT e.id, e.itd, e.dtm, e.cnt, e.dark_time, e.work_time
-    FROM externdb.flow e LEFT JOIN flow m ON m.id == e.id
-    WHERE m.id IS NULL;
-
+INSERT INTO flow SELECT e.* FROM externdb.flow e LEFT JOIN flow m ON m.id == e.id
+  WHERE m.id IS NULL;
 
