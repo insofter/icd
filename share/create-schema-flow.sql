cef0830ac12bc2f99dfdd003d57a6b56bec24e1c
Piotr Mika <piotr.mt.mika@gmail.com>
2012-10-02 12:52:48 +0200

    tables `flow` and `counters`, ex without sync etc

--- share/create-schema-flow.sql
+++ share/create-schema-flow.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS flow;
 
 CREATE TABLE flow (
   id INTEGER PRIMARY KEY,
-  itd TEXT NOT NULL,           -- input transition detector (e.g. photocell) (e.g. itd0, itd1 etc)
+  itd INTEGER NOT NULL,           -- input transition detector (e.g. photocell) (e.g. itd0, itd1 etc)
   dtm INTEGER NOT NULL,        -- the beginning of the entry; Unixtime in secs
   cnt INTEGER DEFAULT 0,       -- number of registered crossings
   dark_time INTEGER DEFAULT 0, -- time the detector was blocked in milisecs
