2daee4f4a2dbd975f7a6c142120b3e8f0aae4d26
Piotr Mika <piotr.mt.mika@gmail.com>
2012-10-17 21:44:34 +0200

    Working flush without removing old records

--- src/icd-flush-db.cpp
+++ src/icd-flush-db.cpp
@@ -3,6 +3,7 @@
 #include "db-config.h"
 #include <cstdlib>
 #include <string.h>
+#include <cstdio>
 
 
 
@@ -41,6 +42,65 @@ int main( int argc, char *argv[] ) {
   globalLiveDb->open( std::getenv("ICD_LIVE_DB") );
   globalLiveDb->busy_timeout( db_timeout );
 
+
+  sqlite3cc::stmt dataDbSql( *globalDataDb );//begin transaction on data.db
+  dataDbSql.prepare( "BEGIN TRANSACTION" );
+  dataDbSql.step();
+  dataDbSql.finalize();
+
+  sqlite3cc::stmt dataDbSqlSelect( *globalDataDb );
+  dataDbSqlSelect.prepare( "SELECT id FROM flow WHERE counter_id == ?1 AND dtm ==?2 LIMIT 1" );
+
+  sqlite3cc::stmt dataDbSqlUpdate( *globalDataDb );
+  dataDbSqlUpdate.prepare( "UPDATE flow SET cnt = ?3, dark_time = ?4, work_time = ?5, flags = ?6 WHERE counter_id == ?1 AND dtm ==?2" );
+
+
+  sqlite3cc::stmt dataDbSqlInsert( *globalDataDb );
+  dataDbSqlInsert.prepare( "INSERT INTO flow (counter_id, dtm, cnt, dark_time, work_time, flags) VALUES( ?1, ?2, ?3, ?4, ?5, ?6 )" );
+
+
+  sqlite3cc::stmt liveDbSqlSelect( *globalLiveDb );
+  liveDbSqlSelect.prepare( "SELECT counter_id, dtm, cnt, dark_time, work_time, flags FROM flow" );
+
+  while( liveDbSqlSelect.step() == SQLITE_ROW ) {
+    // "SELECT id FROM flow WHERE counter_id == ?1 AND dtm ==?2"
+    printf("counter_id=%i, dtm=%i\n", liveDbSqlSelect.column_int(0), liveDbSqlSelect.column_int(1));
+    dataDbSqlSelect.bind_int( 1, liveDbSqlSelect.column_int(0) );
+    dataDbSqlSelect.bind_int( 2, liveDbSqlSelect.column_int(1) );
+    if( dataDbSqlSelect.step() == SQLITE_ROW ) {
+      //"UPDATE flow SET cnt = ?3, dark_time = ?4, work_time = ?5, flags = ?6 WHERE counter_id == ?1 AND dtm ==?2"
+      dataDbSqlUpdate.bind_int( 1, liveDbSqlSelect.column_int(0) );//counter_id
+      dataDbSqlUpdate.bind_int( 2, liveDbSqlSelect.column_int(1) );//dtm
+      dataDbSqlUpdate.bind_int( 3, liveDbSqlSelect.column_int(2) );//cnt
+      dataDbSqlUpdate.bind_int( 4, liveDbSqlSelect.column_int(3) );//dark_time
+      dataDbSqlUpdate.bind_int( 5, liveDbSqlSelect.column_int(4) );//work_time
+      dataDbSqlUpdate.bind_int( 6, liveDbSqlSelect.column_int(5) );//flags
+      dataDbSqlUpdate.step();
+      dataDbSqlUpdate.reset();
+    } else {
+      //"INSERT INTO flow (counter_id, dtm, cnt, dark_time, work_time, flags) VALUES( ?1, ?2, ?3, ?4, ?5, ?6 )"
+      dataDbSqlInsert.bind_int( 1, liveDbSqlSelect.column_int(0) );//counter_id
+      dataDbSqlInsert.bind_int( 2, liveDbSqlSelect.column_int(1) );//dtm
+      dataDbSqlInsert.bind_int( 3, liveDbSqlSelect.column_int(2) );//cnt
+      dataDbSqlInsert.bind_int( 4, liveDbSqlSelect.column_int(3) );//dark_time
+      dataDbSqlInsert.bind_int( 5, liveDbSqlSelect.column_int(4) );//work_time
+      dataDbSqlInsert.bind_int( 6, liveDbSqlSelect.column_int(5) );//flags
+      dataDbSqlInsert.step();
+      dataDbSqlInsert.reset();
+    }
+    dataDbSqlSelect.reset();
+  }
+  liveDbSqlSelect.finalize();
+  dataDbSqlSelect.finalize();
+  dataDbSqlUpdate.finalize();
+  dataDbSqlInsert.finalize();
+
+  dataDbSql.prepare( "COMMIT" );
+  dataDbSql.step();
+  dataDbSql.finalize();
+
+
+//todo rm old records
   /*
    * begin transaction datadb
    * select * from livedb
