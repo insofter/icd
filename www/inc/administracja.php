9f7e49d606c9200b552f56d69fc5effc986c6ab7
Piotr Mika <piotr.mt.mika@gmail.com>
2012-12-17 13:53:59 +0100

    dump /home/data directory to tar file

--- www/inc/administracja.php
+++ www/inc/administracja.php
@@ -1,9 +1,26 @@
 <?php
 defined('INSOFTER') or die('<h1>Your Kung-Fu is too weak.</h1>');
 
-$icdtcp = new c_icdtcp();
+if( isset($_GET['todo']) ) {
+  switch( $_GET['todo'] ) {
+  case 'tar':
+    header('Content-Type: application/octet-stream');
+    header("Pragma: public");
+    header("Expires: 0");
+    header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+    header("Content-Type: application/x-tar");
+    header('Content-Disposition: attachment; filename="'.date('Y.m.d_H.i').'__data.tar"');
 
-$info=$icdtcp->info();
+    passthru('cd /home && tar cf - data/');
+    exit();
+
+    break;
+  }
+
+} else {
+
+
+$icdtcp = new c_icdtcp();
 
 $tresc='<div id="tresc">';
 
@@ -12,8 +29,6 @@ $tresc.='<form action="./?strona=tcpip" method="POST">';
 $tresc.='
   <h3>Administracja</h3>';
 
-//  'plik_konf'=>'Plik konf.'
-//
 $tresc.='
   <ul>
   <li><a onclick="return confirm('."'Czy na pewno chcesz uruchomić ponownie rejestrator?'".');"
@@ -23,7 +38,7 @@ $tresc.='
 
   <li><a href="./?strona=plik_konf">Zrzut bazy do CSV //TODO</a>
 
-  <li><a href="./?strona=plik_konf">Zrzut plików bazy //TODO</a>
+  <li><a href="./?strona=administracja&amp;todo=tar">Zrzut plików bazy (archiwum *.tar)</a>
 
   <li><a onclick="return confirm('."'Czy na pewno oznaczyć rekordy jako wysłane?'".');"
     href="./?strona=plik_konf">Oznacz wszystkie rekordy jako wysłane //TODO</a>
@@ -35,4 +50,6 @@ $tresc.='
   ';
 
 $tresc.='</div>';
+
+}
 ?>
