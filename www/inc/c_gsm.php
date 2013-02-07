4cc2808fc6155251f871fa1571a3a0f584311d46
Piotr Mika <piotr.mt.mika@gmail.com>
2013-02-07 23:55:07 +0100

    gsm www interface

---
+++ www/inc/c_gsm.php
@@ -0,0 +1,62 @@
+<?php
+defined('INSOFTER') or die('<h1>Your Kung-Fu is too weak.</h1>');
+
+class c_gsm {
+
+  var $configDb;
+  var $ICD_SHARE_DIR;
+
+  function __construct() {
+    foreach( file("/etc/profile.d/icd.sh", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES) as $tmp ) {
+      $tmp=explode( '=', strtr( $tmp, array('export '=>'', '$'=>'', '('=>'', ';'=>'', ) ) );
+      eval( '$'.$tmp[ 0 ].'="'.$tmp[ 1 ].'";' );
+    }
+     try { 
+       $this->configDb = new PDO('sqlite:'.$ICD_CONFIG_DB);
+     } catch (PDOException $e) { 
+       print $e->getMessage();
+     } 
+    $this->ICD_SHARE_DIR=$ICD_SHARE_DIR;
+  }
+
+  function disable() {
+    $sql="UPDATE config SET `value`='no' WHERE section_id".
+      "=(SELECT `id` FROM config_section WHERE name='gsm') AND `key`='enabled'";
+    $ans=$this->configDb->query($sql);
+  }
+
+  function pob_modemy() {
+    foreach( scandir( $this->ICD_SHARE_DIR.'/gsm' ) as $provider ) {
+      if( $provider[0]!='.' ) {
+        foreach( scandir( $this->ICD_SHARE_DIR.'/gsm/'.$provider ) as $modem ) {
+          if( $modem[0]!='.' ) {
+            $modemy[$provider][]=$modem;
+          }
+        }
+      }
+    }
+    return $modemy;
+  }
+
+  function pob_ustawienia() {
+    $sql="SELECT `key`, `value` FROM config_section cs LEFT JOIN config c ON cs.id=c.section_id
+      WHERE cs.name='gsm' AND ( c.key='enabled' OR c.key='pin-enabled' 
+      OR c.key='pin' OR c.key='provider' OR c.key='modem' )";
+    $ans=$this->configDb->query($sql);
+    foreach( $ans as $row ) {
+      $ustawienia[$row['key']]=$row['value'];
+    }
+    return $ustawienia;
+  }
+
+  function ustaw($nowe) {
+    foreach( $nowe as $pole=>$wart ) {
+      $sql="UPDATE config SET `value`=".$this->configDb->quote($wart)."
+        WHERE section_id=(SELECT `id` FROM config_section WHERE name='gsm')
+        AND `key`=".$this->configDb->quote($pole);
+      $this->configDb->query($sql);
+    }
+  }
+
+
+}
--- www/inc/gsm.php
+++ www/inc/gsm.php
@@ -1,14 +1,88 @@
 <?php
 defined('INSOFTER') or die('<h1>Your Kung-Fu is too weak.</h1>');
 
-//require_once('inc/c_logi.php');
+require_once('inc/c_gsm.php');
 
+$gsm=new c_gsm();
 //$logi = new c_logi();
 
 
-$tresc='<div id="tresc"><h3>GSM</h3>
+if( isset( $_POST['send'] ) ) {
+  if( $_POST['send']=='Włącz' ) {
+    if( isset( $_POST['pin-enabled'] ) && $_POST['pin']!=$_POST['pin2'] ) {
+      $info='<h4 id="err">Kody PIN nie są zgodne</h4>';
+    } else {
+      $t=explode( '*', $_POST['modem'] );
+      $ustawienia['provider']=$t[0];
+      $ustawienia['modem']=$t[1];
+      $ustawienia['pin']=$_POST['pin'];
+      if( isset( $_POST['pin-enabled'] ) ) {
+        $ustawienia['pin-enabled']='yes';
+      } else {
+        $ustawienia['pin-enabled']='no';
+      }
+      $ustawienia['enabled']='yes';
 
-  </ul></div>';
+      $gsm->ustaw( $ustawienia );
+      $info='<h4>Włączono moduł GSM</h4>';
+    }
+  } else {
+    $gsm->disable();
+    $info='<h4>Wyłączono moduł GSM</h4>';
+  }
+}
+
+
+$ustawienia=$gsm->pob_ustawienia();
+
+$modemy=$gsm->pob_modemy();
+
+$tresc='<div id="tresc"><h3>GSM</h3>';
+if( isset( $info ) ) {
+  $tresc.=$info;
+}
+$tresc.='<form action="./?strona=gsm" method="POST">
+  <table>
+  <tr><th>Stan modułu GSM : </th><th>';
+if( $ustawienia['enabled']=='yes' ) {
+  $tresc.='aktywny';
+} else {
+  $tresc.='nieaktywny';
+}
+
+$tresc.='</th></tr><tr><th><label for="modem">Wybierz modem i dostawcę</label> :</th><td>
+  <select name="modem" id="modem">';
+foreach( $modemy as $provider=>$modem ) {
+  $tresc.='<optgroup label="'.$provider.'">';
+  foreach( $modem as $typ ) {
+    if( $provider==$ustawienia['provider'] && $typ==$ustawienia['modem'] ) {
+      $tresc.='<option value="'.$provider.'*'.$typ.'" selected="selected">'.$typ.' ('.$provider.')</option>';
+    } else {
+      $tresc.='<option value="'.$provider.'*'.$typ.'">'.$typ.' ('.$provider.')</option>';
+    }
+  }
+  $tresc.='</optgroup>';
+}
+$tresc.='</select></td></tr>
+  <tr><th><label for="pin-enabled">Włącz PIN</label> :</th><td>
+  <input type="checkbox" name="pin-enabled" value="pin-enabled" id="pin-enabled"';
+if( $ustawienia['pin-enabled']=='yes' ) {
+  $tresc.='checked="checked"';
+}
+
+$tresc.='>
+  <tr><th><label for="pin">PIN</label> :</th><td>
+  <input type="text" name="pin" id="pin" value="'.$ustawienia['pin'].'" size="6"></td></tr>
+  <tr><th><label for="pin2">powtórz PIN</label> :</th><td>
+  <input type="text" name="pin2" id="pin2" value="*pin*" size="6"></td></tr>
+  <tr><th colspan="2">
+  <input type="submit" name="send" value="Wyłącz">
+  <input type="submit" name="send" value="Włącz">
+  </th></tr>
+  </table>
+  </form>';
+
+$tresc.='</div>';
 
 
 ?>
