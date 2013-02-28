3daf2f0e48e59c9bf57b22e3d443dca15a7ad69f
Piotr Mika <piotr.mt.mika@gmail.com>
2013-02-28 17:12:52 +0100

    GREAT REPO CLEANUP!!!!

--- share/init-last-aggr-time.sql
+++
@@ -1,5 +0,0 @@
-INSERT OR IGNORE INTO config (section_id, key, value) VALUES (
-  (SELECT id FROM config_section WHERE name == 'current'),
-  'last-aggr-time',
-  (SELECT CAST(strftime('%s', 'now') AS INTEGER))
-);
--- share/list-tests.sql
+++
@@ -1,2 +0,0 @@
-SELECT * FROM tests ORDER BY id ASC;
-
